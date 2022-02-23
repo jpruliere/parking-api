eadf84f23511e811a7e7ab39978d382a571ec679
trogibae <jeanpruliere@gmail.com>
2022-02-23 14:53:31 +0100

    premier endpoint de l'API

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 require('dotenv').config();
-// serveur Express
+const express = require('express');
+const app = express();
+
+const port = 9900;
+
+// REST
+// les verbes HTTP ont un rôle
+// GET : récupérer des données, lecture
+// POST : insérer des données, écriture
+// PUT : modifier des données, écriture
+// PATCH : modifier des données, écriture
+// DELETE : supprimer des données, écriture
+
+// les endpoints désignent ce qu'ils concernent
+// GET /parkings doit retourner des parkings, pas des chantiers
+// DELETE /maintenances/14 doit servir à supprimer le chantier dont l'id est 14, pas à supprimer le véhicule dont l'id est 14
+
+// les noms des ressources sont au pluriel
+// /parkings et pas /parking
+// /visits et pas /visit
+
+const parking = require('./requests/parking');
+
+app.get('/parkings', async (req, res) => {
+  const parkings = await parking.findAll();
+  res.json(parkings);
+})
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
